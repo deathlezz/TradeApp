7db8b6da8372acbb9bbacd14f4e5dfceaa5f9b40
deathlezz <deathlezz@wp.pl>
2022-11-17 14:22:47 +0100

    Initial Commit

---
+++ TradeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradeApp
+//
+//  Created by deathlezz on 17/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
