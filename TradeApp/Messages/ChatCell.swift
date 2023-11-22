2027aebdfbec6dbeccf85a7d994fed0706a781df
deathlezz <deathlezz@wp.pl>
2023-11-22 22:23:23 +0000

    Created custom TableViewCell class for chats

---
+++ TradeApp/Messages/ChatCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatCell.swift
+//  TradeApp
+//
+//  Created by deathlezz on 22/11/2023.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
