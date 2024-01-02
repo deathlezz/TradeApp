275e49b249817f292476add339e574007af40803
deathlezz <deathlezz@wp.pl>
2024-01-02 13:49:27 +0000

    Added unit tests

--- TradeAppTests/ActiveAdsViewTests.swift
+++ TradeAppTests/ActiveAdsViewTests.swift
@@ -6,30 +6,26 @@
 //
 
 import XCTest
+@testable import TradeApp
 
 final class ActiveAdsViewTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // check if item expiry date is correct
+    func testSuccessSetExpiryDate() {
+        // Given (Arrange)
+        let userCalendar = Calendar.current
+        let expiryDate = userCalendar.date(byAdding: .day, value: 30, to: Date())!
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en")
+        dateFormatter.dateFormat = "MMMM d"
+        let formattedExpiryDate = dateFormatter.string(from: expiryDate)
+        let vc = ActiveAdsView()
+        // When (Act)
+        let result = vc.setExpiryDate(Date())
+        // Then (Assert)
+        XCTAssertEqual(result, "expires \(formattedExpiryDate)")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
+    
 
 }
